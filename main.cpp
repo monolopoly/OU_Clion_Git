eadf2a4f396983b779f349add03c6ca0a71a79bc
Paulina <paulinag.027@gmail.com>
2021-02-08 00:14:34 +0100

    Zmiany dodatkowe w 03

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int Policz (int a, int b) {
-    return a*b;
+    return (a+b)*(a-b);
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W drugim branchu 2 * 3 = " << Policz(2, 3) << std::endl;
+    std::cout << "W trzecim branchu (2 + 3) * (2 - 3) = " << Policz(2, 3) << std::endl;
     return 0;
 }
